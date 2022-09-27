6992bad6e58cdeca6eaadcee872399a42bbe4c3f
Harald Vöhringer <harald.voeh@gmail.com>
2022-09-27 17:59:19 +0200

    login doesnt work

--- app.py
+++ app.py
@@ -18,56 +18,56 @@ if "authentication_status" not in st.session_state:
 
 
 # authentication
-name, authentication_status, username = authenticator.login("Login")
-if authentication_status == False:
-    st.error("Username/Password is incorrect.")
+# name, authentication_status, username = authenticator.login("Login")
+# if authentication_status == False:
+#     st.error("Username/Password is incorrect.")
 
-if authentication_status is None:
-    st.warning("Please enter your username and password.")
+# if authentication_status is None:
+#     st.warning("Please enter your username and password.")
 
-if authentication_status:
-    ds = read_file(st.session_state["current_file"])
+# if authentication_status:
+ds = read_file(st.session_state["current_file"])
 
-    with st.sidebar:
-        authenticator.logout("Logout")
-        with st.form("image_view"):
-            xmin, xmax = st.slider(
-                "Select x-range:",
-                value=[500, 1000],
-                key="xrange",
-                step=1,
-                min_value=0,
-                max_value=ds.dims["x"],
-            )
-            st.write("You are viewing ", xmax - xmin, "pixels along the x-axis.")
+with st.sidebar:
+    authenticator.logout("Logout")
+    with st.form("image_view"):
+        xmin, xmax = st.slider(
+            "Select x-range:",
+            value=[500, 1000],
+            key="xrange",
+            step=1,
+            min_value=0,
+            max_value=ds.dims["x"],
+        )
+        st.write("You are viewing ", xmax - xmin, "pixels along the x-axis.")
 
-            ymin, ymax = st.slider(
-                "Select y-range:",
-                value=[500, 1000],
-                key="yrange",
-                step=1,
-                min_value=0,
-                max_value=ds.dims["y"],
-            )
-            st.write("You are viewing ", ymax - ymin, "pixels along the y axis.")
+        ymin, ymax = st.slider(
+            "Select y-range:",
+            value=[500, 1000],
+            key="yrange",
+            step=1,
+            min_value=0,
+            max_value=ds.dims["y"],
+        )
+        st.write("You are viewing ", ymax - ymin, "pixels along the y axis.")
 
-            fig = plain_img_box(ds, xmin, xmax, ymin, ymax)
+        fig = plain_img_box(ds, xmin, xmax, ymin, ymax)
 
-            st.sidebar.pyplot(fig)
+        st.sidebar.pyplot(fig)
 
-            current_panel = st.selectbox(
-                "Select a pannel.",
-                PANELS.keys(),
-            )
+        current_panel = st.selectbox(
+            "Select a pannel.",
+            PANELS.keys(),
+        )
 
-            current_channels = st.multiselect(
-                "Select the channels:",
-                options=CHANNELS,
-                default=PANELS[current_panel],
-            )
+        current_channels = st.multiselect(
+            "Select the channels:",
+            options=CHANNELS,
+            default=PANELS[current_panel],
+        )
 
-            submitted = st.form_submit_button("Submit")
+        submitted = st.form_submit_button("Submit")
 
-    st.header("CODEX Viewer")
-    fig = plain_img(ds, current_channels, xmin, xmax, ymin, ymax)
-    st.pyplot(fig)
+st.header("CODEX Viewer")
+fig = plain_img(ds, current_channels, xmin, xmax, ymin, ymax)
+st.pyplot(fig)
